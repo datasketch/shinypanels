a3135fa09e2be94ebb632a9dc9ac03a45b39eba5
CamilaAchury <camilaachury.4@gmail.com>
2019-03-14 18:43:21 -0500

    cambios temporales

--- R/layout.R
+++ R/layout.R
@@ -25,9 +25,9 @@ dsAppPage <- function(dataControls, dataPreview,
 
   page <- tagList(
 
-      div(class="app-container",
-      div(class="panel is-collapsable box-shadow top-malibu is-collapsed has-settings", id="data-edit",
-      HTML(paste0('
+    div(class="app-container",
+        div(class="panel is-collapsable box-shadow top-malibu is-collapsed has-settings", id="data-edit",
+            HTML(paste0('
       <div class="panel-head">
       <div class="panel-title text-malibu">Editar datos</div>
       <svg class="icon-close icon-close--malibu" xmlns="http://www.w3.org/2000/svg" width="10" height="10">
@@ -35,15 +35,15 @@ dsAppPage <- function(dataControls, dataPreview,
       <line x1="10" y1="0" x2="0" y2="10" />
       </svg>
       </div>'
-      )),
-      div(class="panel-body",
-      div(class="panel-content",
-          dataControls
-      )
-      )
-      ),
-      div(class="panel is-collapsable box-shadow top-malibu panel--expanded", id="data-edit",
-      HTML(paste0('
+            )),
+            div(class="panel-body",
+                div(class="panel-content",
+                    dataControls
+                )
+            )
+        ),
+        div(class="panel is-collapsable box-shadow top-malibu panel--expanded", id="data-prev",
+            HTML(paste0('
       <div class="panel-head">
       <div class="panel-title text-malibu">Ver datos</div>
       <svg class="icon-close icon-close--malibu" xmlns="http://www.w3.org/2000/svg" width="10" height="10">
@@ -51,14 +51,15 @@ dsAppPage <- function(dataControls, dataPreview,
       <line x1="10" y1="0" x2="0" y2="10" />
       </svg>
       </div>')),
-      div(class="panel-body",
-      div(class="panel-content",
-          dataPreview
-      )
-      )
-      ),
-      div(class="panel is-collapsable box-shadow top-chardonnay is-collapsed has-settings", id="data-edit",
-      HTML(paste0('
+            div(class="panel-body",
+                div(class="panel-content",
+                    dataPreview
+                )
+            )
+        ),
+        div(class="panel is-collapsable box-shadow top-malibu panel--expanded", id="viz-edit",
+            #div(class="panel is-collapsable box-shadow top-chardonnay is-collapsed has-settings", id="data-edit",
+            HTML(paste0('
       <div class="panel-head">
       <div class="panel-title text-chardonnay">Editar visualización</div>
       <svg class="icon-close icon-close--chardonnay" xmlns="http://www.w3.org/2000/svg" width="10" height="10">
@@ -66,31 +67,31 @@ dsAppPage <- function(dataControls, dataPreview,
       <line x1="10" y1="0" x2="0" y2="10" />
       </svg>
       </div>'
-       )),
-      div(class="panel-body",
-      div(class="panel-content",
-          vizControls
-      )
-      )
-      ),
-      div(class="panel is-not-collapsable top-chardonnay", id="data-edit",
-      div(class="panel-head",
-      div(class="panel-title text-chardonnay", "Vista previa"),
-      tags$a(href="", class="btn", "Publicar")
-      ),
-      div(class="panel-body box-shadow",
-      div(class="panel-content",
-          vizPreview
-      )
-      ),
-      div(class="panel-footer box-shadow",
-      div(class="panel-title", "Tipos de visualización"),
-      div(class="viz-types",
-          vizIcons
-      )
-      )
-      )
-      ),
+            )),
+            div(class="panel-body",
+                div(class="panel-content",
+                    vizControls
+                )
+            )
+        ),
+        div(class="panel is-not-collapsable top-chardonnay", id="viz-prev",
+            div(class="panel-head",
+                div(class="panel-title text-chardonnay", "Vista previa"),
+                tags$a(href="", class="btn", "Publicar")
+            ),
+            div(class="panel-body box-shadow",
+                div(class="panel-content",
+                    vizPreview
+                )
+            ),
+            div(class="panel-footer box-shadow",
+                div(class="panel-title", "Tipos de visualización"),
+                div(class="viz-types",
+                    vizIcons
+                )
+            )
+        )
+    ),
     debugJS,
     indexJS,
     stepsCSS(styles)
@@ -101,65 +102,60 @@ dsAppPage <- function(dataControls, dataPreview,
 }
 
 
-#' @export
-dataControls <- function(..., label = NULL, on = NULL){
-  x <- list(...)
-  tagList(
-    shiny::tags$form(class="settings data__settings",
-                     shiny::tags$fieldset(
-                       shiny::tags$legend(label),
-                       x
-                     )
-    )
-  )
-}
+#' #' @export
+#' dataControls <- function(..., label = NULL, on = NULL){
+#'   x <- list(...)
+#'   tagList(
+#'     shiny::tags$form(class="settings data__settings",
+#'                      shiny::tags$fieldset(
+#'                        shiny::tags$legend(label),
+#'                        x
+#'                      )
+#'     )
+#'   )
+#' }
+#'
+#' #' @export
+#' dataPreview <- function(..., on = NULL){
+#'   x <- list(...)
+#'   tagList(
+#'     x
+#'   )
+#' }
 
-#' @export
-dataPreview <- function(..., on = NULL){
-  x <- list(...)
-  tagList(
-    x
-  )
-}
+#' #' @export
+#' vizControls <- function(...,
+#'                         basic = NULL,
+#'                         advanced = NULL,
+#'                         label = NULL, on = NULL){
+#'   x <- list(...)
+#'   tagList(
+#'     tags$ul(class = "ds__tabs",
+#'             tags$li(class = "ds__tab active", `data-tab`="options__general","General"),
+#'             tags$li(class = "ds__tab", `data-tab`="options__advanced","Avanzado")
+#'     ),
+#'     div(id="options__general", class="ds__tab-content-pane active",
+#'         div(class="flex", style="border-bottom: 1px solid var(--disco); padding: 5px 0; margin: 0 0 10px;",
+#'             basic
+#'         )
+#'     ),
+#'     div(id="options__advanced", class="ds__tab-content-pane",
+#'         div(class="flex", style="border-bottom: 1px solid var(--disco); padding: 5px 0; margin: 0 0 10px;",
+#'             advanced
+#'         )
+#'     ),
+#'     x
+#'   )
+#' }
 
-#' @export
-vizControls <- function(...,
-                        basic = NULL,
-                        advanced = NULL,
-                        label = NULL, on = NULL){
-  x <- list(...)
-  tagList(
-    shiny::tags$form(class="settings viz__settings",
-                     shiny::tags$fieldset(
-                       shiny::tags$legend(label),
-                       tags$ul(class = "ds__tabs",
-                               tags$li(class = "ds__tab active", `data-tab`="options__general","General"),
-                               tags$li(class = "ds__tab", `data-tab`="options__advanced","Avanzado")
-                       ),
-                       div(id="options__general", class="ds__tab-content-pane active",
-                           div(class="flex", style="border-bottom: 1px solid var(--disco); padding: 5px 0; margin: 0 0 10px;",
-                               basic
-                           )
-                       ),
-                       div(id="options__advanced", class="ds__tab-content-pane",
-                           div(class="flex", style="border-bottom: 1px solid var(--disco); padding: 5px 0; margin: 0 0 10px;",
-                               advanced
-                           )
-                       ),
-                       x
-                     )
-    )
-  )
-}
-
-#' @export
-vizPreview <- function(..., on = NULL){
-  x <- list(...)
-  tagList(
-    x
-  )
-}
-
+#' #' @export
+#' vizPreview <- function(..., on = NULL){
+#'   x <- list(...)
+#'   tagList(
+#'     x
+#'   )
+#' }
+#'
 stepsCSS <- function(styles = ""){
   tags$style(
     styles
--- inst/examples.R
+++ inst/examples.R
@@ -2,21 +2,24 @@ library(dsAppLayout)
 library(dsAppWidgets)
 library(hgchmagic)
 
-ui <- dsAppPage(dataControls = HTML("Controles data"),
-                dataPreview =
-                dataPreview(
-                  h3("This is data preview"),
-                  dsHot("dataTable", data = diamonds),
-                  verbatimTextOutput("debug_data")
-                ),
-                vizControls = "controles",
-                vizPreview = highchartOutput("viz") ,
-                vizIcons = "zona de iconos")
+ui <- dsAppPage(dataControls = "ja",
+                dataPreview = "hola",
+                vizControls = uiOutput("bla"),
+                vizPreview = highchartOutput("viz"),
+                vizIcons = verbatimTextOutput("test"))
 
 server <- function(input, output, session) {
 
   output$viz <- renderHighchart({
     hgch_bar_CatNum(sampleData("Cat-Num", 100))
+  })
+
+  output$bla <- renderUI({
+    selectizeInput("asd", "David es un gil", c("Si", "obvio", "Sin duda"))
+  })
+
+  output$test <- renderPrint({
+    "test icon viz"
   })
 
 }
