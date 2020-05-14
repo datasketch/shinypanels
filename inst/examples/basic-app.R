a342ce80bb23e439bd75369d1872c360e81a2f41
Juan Pablo Marin Diaz <jpmarindiaz@gmail.com>
2020-05-13 22:34:08 -0500

    added pre tag styles for debug text

--- inst/examples/basic-app.R
+++ inst/examples/basic-app.R
@@ -6,12 +6,14 @@ ui <- panelsPage(
     width = 300,
     color = "magenta",
     body = div(
-
-    )
-  ),
-  panel(
-    title = "Panel 2",
-    body = div(
+      h3("Here is some info"),
+      verbatimTextOutput("debug"),
+      p("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
     )
   ),
   panel(
@@ -19,7 +21,7 @@ ui <- panelsPage(
     width = 350,
     collapsed = TRUE,
     body = div(
-
+      plotOutput("plot")
     )
   ),
   panel(
@@ -34,6 +36,14 @@ ui <- panelsPage(
 
 
 server <-  function(input, output, session) {
+
+  output$debug <- renderPrint({
+    cat(paste(c("La", "casa", "en", "el", "árbol"), collapse = "\n"))
+  })
+  output$plot <- renderPlot(
+    plot(cars)
+  )
+
 }
 
 shinyApp(ui, server)
